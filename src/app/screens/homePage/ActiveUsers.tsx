b123f1fae565e101b18ff97b7a8249c2460e8e7f
ali221022 <shohjahon.juraev.4496@mail.ru>
2024-10-11 19:37:38 +0900

    BRR: feat: develop events sectional component

--- src/app/screens/homePage/ActiveUsers.tsx
+++ src/app/screens/homePage/ActiveUsers.tsx
@@ -1,10 +1,60 @@
 import React from "react";
-import { Container } from "@mui/material";
+import { Container, Box, Stack } from "@mui/material";
+import Card from "@mui/joy/Card";
+import { CssVarsProvider } from "@mui/joy";
+import CardOverflow from "@mui/joy/CardOverflow";
+import AspectRatio from "@mui/joy/AspectRatio";
+
+import CardContent from "@mui/joy/CardContent";
+import Divider from "@mui/joy/Divider";
+import Typography from "@mui/joy/Typography";
+import IconButton from "@mui/joy/IconButton";
+import Link from "@mui/joy/Link";
+import Favorite from "@mui/icons-material/Favorite";
+
+
+const activeUsers = [
+  { memberNick: "Martin", memberImage: "/img/martin.webp" },
+  { memberNick: "Justin", memberImage: "/img/justin.webp" },
+  { memberNick: "Rose", memberImage: "/img/rose.webp" },
+  { memberNick: "Nusret", memberImage: "/img/nusret.webp" },
+];
 
 export default function ActiveUsers() {
-    return (
-    <div>
-       <Container>ActiveUsers</Container>
+  return (
+    <div className={"active-users-frame"}>
+      <Container>
+        <Stack className={"main"}>
+          <Box className={"category-title"}>Active Users</Box>
+          <Stack className={"cards-frame"}>
+            <CssVarsProvider>
+              {activeUsers.length !== 0 ? (
+                activeUsers.map((ele, index) => {
+                  return (
+                    <Card key={index} variant="outlined" className={"card"}>
+                      <CardOverflow>
+                        <AspectRatio ratio="1">
+                          <img src={ele.memberImage} alt="" />
+                        </AspectRatio>
+                      </CardOverflow>
+
+                      <CardOverflow  className="">
+                        <Stack className={"info"}>
+                          <Typography className={"member-nickname"}>
+                            {ele.memberNick}
+                          </Typography>
+                        </Stack>
+                      </CardOverflow>
+                    </Card>
+                  );
+                })
+              ) : (
+                <Box className={"no-data"}>No Active Users!</Box>
+              )}
+            </CssVarsProvider>
+          </Stack>
+        </Stack>
+      </Container>
     </div>
-    );
- }
+  );
+}
