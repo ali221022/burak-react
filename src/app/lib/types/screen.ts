1967af6b3494dac6b006008f2be05d9369b06955
ali221022 <shohjahon.juraev.4496@mail.ru>
2024-10-30 21:48:39 +0900

    BRR: feat:develop AppRootState and HomePage type integrations

---
+++ src/app/lib/types/screen.ts
@@ -0,0 +1,20 @@
+import { Member } from "./member";
+import { Product } from "./product";
+
+/** REACT APP STATE */
+export interface AppRootState {
+    homePage: HomePageState;
+}
+
+
+/** HOME PAGE */
+export interface HomePageState {
+    popularDishes: Product[];
+    newDished: Product[];
+    topUsers: Member[];
+}
+
+/** PRODUCTS PAGE */
+
+
+/** ORDERS PAGE */
