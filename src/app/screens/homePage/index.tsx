66b5054d9c7a6a196336f17d393ba0cfc996fe06
ali221022 <shohjahon.juraev.4496@mail.ru>
2024-10-31 18:14:08 +0900

    BRR: feat: develop slice and selector usage

--- src/app/screens/homePage/index.tsx
+++ src/app/screens/homePage/index.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Statistics from "./Statistics";
 import PopularDishes from "./PopularDishes";
 import NewDishes from "./NewDishes";
@@ -7,8 +7,31 @@ import ActiveUsers from "./ActiveUsers";
 import Events from "./Events";
 import "../../../css/home.css";
 
-export default function HomePage() {
+import { useDispatch, useSelector } from "react-redux";
+import { Dispatch } from "@reduxjs/toolkit";
+import { createSelector } from "reselect";
+import { setPopularDishes } from "./slice";
+import { retrievePopularDishes } from "./selector";
+import { Product } from "../../lib/types/product";
 
+/** REDUX SLICE & SELECTOR */
+
+const actionDispatch = (dispatch: Dispatch) => ({
+   setPopularDishes: (data: Product[]) => dispatch(setPopularDishes(data)),
+});
+
+export default function HomePage() {
+ const { setPopularDishes } = actionDispatch(useDispatch());
+ // Selector: Store => Data
+
+ useEffect(() => {
+   // Backend server data request => Data
+   const result = [{}]; 
+
+   //Slice: Data => Store
+   // @ts-ignore
+   setPopularDishes(result);
+ }, []);
      
 
     return <div className={"home-page"}>
