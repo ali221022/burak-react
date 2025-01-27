dccf3155455c19e334bcd5ed6814efc239918b11
ali221022 <shohjahon.juraev.4496@mail.ru>
2025-01-28 04:14:02 +0900

    BRR: feat: develop SocketIO client

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,6 +1,5 @@
 import React, { useState } from "react";
-import { Box, Button, Container, Stack, Typography } from "@mui/material";
-import { Link, Route, Switch, useLocation } from "react-router-dom";
+import { Route, Switch, useLocation } from "react-router-dom";
 import HomePage  from "./screens/homePage";
 import ProductsPage  from "./screens/productsPage";
 import OrdersPage  from "./screens/ordersPage";
@@ -9,15 +8,15 @@ import HomeNavbar  from "./components/headers/HomeNavbar";
 import OtherNavbar  from "./components/headers/OtherNavbar";
 import Footer  from "./components/footer";
 import HelpPage  from "./screens/helpPage";
-import "../css/app.css";
-import "../css/navbar.css";
-import "../css/footer.css"
 import useBasket from "./hooks/useBasket";
 import AuthenticationModal from "./components/auth";
 import { sweetErrorHandling, sweetTopSuccessAlert } from "./lib/sweetAlert";
 import { Messages } from "./lib/config";
 import MemberService from "./services/MemberService";
 import { useGlobals } from "./hooks/useGlobal";
+import "../css/app.css";
+import "../css/navbar.css";
+import "../css/footer.css"
 
 
 function App() {
